46ea196759e2a3968694171d0442ab487a1cc1a3
asseternity <nakupovasset@gmail.com>
2024-09-01 19:19:11 +0500

    fix: set port in an environment variable

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/', indexRoute);
 app.use('/new', newRoute);
 
 // activating the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => { console.log(`Message board active and listening on port: ${PORT}!`) });
 
 // testing
